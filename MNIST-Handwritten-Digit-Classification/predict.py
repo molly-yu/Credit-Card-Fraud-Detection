4ccac615310badd0fc4cab433b2e095b5fdff551
Molly Yu <m234yu@uwaterloo.ca>
2020-08-06 15:34:56 -0400

    Update predict.py

--- MNIST-Handwritten-Digit-Classification/predict.py
+++ MNIST-Handwritten-Digit-Classification/predict.py
@@ -2,6 +2,7 @@
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
+from matplotlib import pyplot
 
 # load and prepare image
 def load_image(filename):
@@ -20,6 +21,8 @@ def load_image(filename):
 def predict():
     # load
     img = load_image('sample.png')
+    pyplot.imshow(img.squeeze())
+    pyplot.show()
     model = load_model('final_model.h5')
     # predict
     digit = model.predict_classes(img)
